8b31656d7216951e233243837ded622e602c46fd
liban <libanamd@gmail.com>
2024-05-02 16:13:04 +0200

    Commit de test

--- src/objet/Etudiant.java
+++ src/objet/Etudiant.java
@@ -5,4 +5,13 @@ public class Etudiant {
 	private String prenom;
 	private String DateN;
 	private String adressemail;
+	private int NumE;
+	public Etudiant(String nom, String prenom, String dateN, String adressemail, int numE) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		DateN = dateN;
+		this.adressemail = adressemail;
+		NumE = numE;
+	}
 }
